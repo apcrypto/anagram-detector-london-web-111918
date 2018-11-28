80b9451d974322665458416e02892515065a4970
Andy <andypurbrick@gmail.com>
2018-11-28 08:42:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+@word = word
+  end
+
+def match(anagrams)
+  answer = []
+  anagrams.each do |element|
+    if element.split("").sort == @word.split("").sort
+      answer << element
+    end
+  end
+  answer
+end
+end
 # Your code goes here!
